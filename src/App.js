f55bd838d10c52a656d28a00df25907e890140ae
Redford Sumcad <redford.sumcad@sc.com>
2021-08-11 23:18:01 +0800

    filter updated

--- src/App.js
+++ src/App.js
@@ -1,63 +1,109 @@
-import React, { useEffect, useState } from 'react';
-import { Modal, Input,Button } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Modal, Input,Button,Dropdown,Dimmer,Loader,Segment } from 'semantic-ui-react';
 import axios from 'axios'
 
 const App = (props) => {
-    const [amt, setAmt] = useState(300);
+    const [amt, setAmt] = useState("");
     const [data, setData] = useState([]);
-    const [open, setOpen] = useState(false)
-
-    const DEFAULT_HEADER = () => {
-        return { headers:
-            {
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50SWQiOjE5NzI1ODYsImFjdGl2YXRlZCI6dHJ1ZSwicm9uaW5BZGRyZXNzIjoiMHg1ZWFiNmExNDdmOTk0YWUxZmMxN2EzNTc1MDZmMWIwMTY3ZjUxMjE4IiwiZXRoQWRkcmVzcyI6bnVsbCwiaWF0IjoxNjI4NTI2MjMxLCJleHAiOjE2MjkxMzEwMzEsImlzcyI6IkF4aWVJbmZpbml0eSJ9.99vK37aEmMvXKJKlvQjl4AmZ_xRYHBgxG4IrTErgX3I"
-            },
-            timeout:120000
-        }
-    }
+    const [open, setOpen] = useState(false);
+    const [breed, setBreed] = useState("any");
+    const [classAxie, setClassAxie] = useState("any");
+    const [isLoading, setIsLoading] = useState(false);
+ 
     const DEFAULT_PAYLOAD = () => {
         return {"operationName":"GetAxieLatest","variables":{"from":0,"size":100,"sort":"Latest","auctionType":"Sale"},"query":"query GetAxieLatest($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    total\n    results {\n      ...AxieRowData\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieRowData on Axie {\n  id\n  image\n  class\n  name\n  genes\n  owner\n  class\n  stage\n  title\n  breedCount\n  level\n  parts {\n    ...AxiePart\n    __typename\n  }\n  stats {\n    ...AxieStats\n    __typename\n  }\n  auction {\n    ...AxieAuction\n    __typename\n  }\n  __typename\n}\n\nfragment AxiePart on AxiePart {\n  id\n  name\n  class\n  type\n  specialGenes\n  stage\n  abilities {\n    ...AxieCardAbility\n    __typename\n  }\n  __typename\n}\n\nfragment AxieCardAbility on AxieCardAbility {\n  id\n  name\n  attack\n  defense\n  energy\n  description\n  backgroundUrl\n  effectIconUrl\n  __typename\n}\n\nfragment AxieStats on AxieStats {\n  hp\n  speed\n  skill\n  morale\n  __typename\n}\n\nfragment AxieAuction on Auction {\n  startingPrice\n  endingPrice\n  startingTimestamp\n  endingTimestamp\n  duration\n  timeLeft\n  currentPrice\n  currentPriceUSD\n  suggestedPrice\n  seller\n  listingIndex\n  state\n  __typename\n}\n"}
     }
 
-    const search = (e) => {
-        e.preventDefault();
+    const search = (amt,breed,classAxie) => {
         const payload = DEFAULT_PAYLOAD();
-        const header = DEFAULT_HEADER();
-
+        setIsLoading(true);
         axios.post('https://axieinfinity.com/graphql-server-v2/graphql', payload)
             .then(async res => {
-                const result = res.data.data.axies.results.filter((obj) => {
-                    console.log(obj.auction.currentPriceUSD);
-                    console.log(amt);
+                let result = res.data.data.axies.results.filter((obj) => {
                     return parseFloat(obj.auction.currentPriceUSD) <= amt;
+                });
+                if(breed !== "any"){
+                    result = result.filter((obj) => {
+                        return parseFloat(obj.breedCount) === breed;
+                    });
                 }
-            );
-            console.log(result);
+                if(classAxie !== "any"){
+                    result = result.filter((obj) => {
+                        return obj.class === classAxie;
+                    });
+                }
 
                 setData(result);
+                setIsLoading(false);
             })
             .catch(err => {
-                setOpen(true)
+                setOpen(true);
+                setIsLoading(false);
             })
     }
     const onChangeAmt = (e) => {
         e.preventDefault();
         setAmt(e.target.value);
+        search(e.target.value,breed,classAxie);
     }
+
+    const breedOptions = [
+        { key: 1, text: 'Any', value: "any" },
+        { key: 2, text: '0', value: 0 },
+        { key: 3, text: '1', value: 1 },
+        { key: 4, text: '2', value: 2 },
+        { key: 5, text: '3', value: 3 }
+    ]
+    const classOptions = [
+        { key: 1, text: 'Any', value: "any" },
+        { key: 2, text: 'Aquatic', value: 'Aquatic' },
+        { key: 3, text: 'Beast', value: 'Beast'},
+        { key: 4, text: 'Plant', value:'Plant' },
+        { key: 5, text: 'Bird', value: 'Bird' },
+        { key: 6, text: 'Bug', value: 'Bug' },
+        { key: 7, text: 'Reptile', value: 'Reptile' }
+    ]
+    const handleDropdownBreed = (e, { value }) => {
+        setBreed(value);
+        search(amt,value,classAxie);
+    }
+    const handleDropdownClass = (e, { value }) => {
+        setClassAxie(value);
+        search(amt,breed,value);
+    }
+
   return (
     <div className="app">
     <h3>Search the lowest price of axies on the market!</h3>
-    <Input
-        placeholder="Enter amount in USD"
-        value={amt}
-        onChange={onChangeAmt}
-      />
-      <button className="ui primary button" type="button"  onClick={search}>Filter</button>
+    <div className="filter-box">
+        <Input
+            placeholder="Enter amount in USD"
+            onBlur={onChangeAmt}
+          />
+          <Dropdown
+              icon='caret down'
+              placeholder="Select Breed Count"
+              selection
+              selectOnBlur={false}
+              options={breedOptions}
+              value={breed}
+              onChange={handleDropdownBreed}
+          />
+          <Dropdown
+              icon='caret down'
+              placeholder="Select Class"
+              selection
+              selectOnBlur={false}
+              options={classOptions}
+              value={classAxie}
+              onChange={handleDropdownClass}
+          />
+      </div>
 
       <table className="ui celled table">
           <thead className="">
           <tr className="">
-              <th className="">id</th>
+              <th className="">ID</th>
               <th className="">Price USD</th>
               <th className="">Class</th>
               <th className="">Breed Count</th>
@@ -65,21 +111,28 @@ const App = (props) => {
           </tr>
       </thead>
       <tbody className="">
+
       {
-          data?.map((obj,i) => {
-              return (
-                  <tr  key={i}>
-                      <td className=""><a href={"https://marketplace.axieinfinity.com/axie/"+obj.id} target="_blank">{obj.id}</a></td>
-                      <td className="">{obj.auction.currentPriceUSD}</td>
-                      <td className="">{obj.class}</td>
-                      <td className="">{obj.breedCount}</td>
-                      <td className=""><img src={obj.image}width="50"/></td>
-                  </tr>
-              )
-          })
+          data.length>1 ?
+              data.map((obj,i) => {
+                  return (
+                      <tr  key={i}>
+                          <td className=""><a rel="noreferrer" href={"https://marketplace.axieinfinity.com/axie/"+obj.id} target="_blank">{obj.id}</a></td>
+                          <td className="">{obj.auction.currentPriceUSD}</td>
+                          <td className="">{obj.class}</td>
+                          <td className="">{obj.breedCount}</td>
+                          <td className=""><img alt="" src={obj.image} width="40"/></td>
+                      </tr>
+                  )
+              })
+              : <tr><td>No records Found!</td></tr>
       }
       </tbody>
       </table>
+
+      <Dimmer className={isLoading ? "active": ""}>
+       <Loader content='Loading' />
+     </Dimmer>
       <Modal
       centered={false}
       open={open}
